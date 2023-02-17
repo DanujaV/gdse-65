4bbdb1c58e34b7c9f1f4944bcaea06dac4a67fb0
danu <danujagreru@gmail.com>
2023-02-17 15:06:25 +0530

    StackOverFlowError

---
+++ exception-04/error/src/ErrorDemo.java
@@ -0,0 +1,20 @@
+/*
+    @author DanujaV
+    @created 2/17/23 - 2:58 PM   
+*/
+
+public class ErrorDemo {
+    private static void methodA() {
+        System.out.println("methodA()");
+        methodB();
+    }
+
+    private static void methodB() {
+        System.out.println("methodB()");
+        methodA();
+    }
+
+    public static void main(String[] args) {
+        methodA();
+    }
+}
